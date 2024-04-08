91493de95a1d2c7e327cf6da1a5bf4b5a151f4e6
Alejandro Ceron <alejandrocg@switchs.co>
2024-04-08 10:23:41 -0500

    FIX: eliminated constructor and added anotation AllArgsConstructor

--- aforo/src/main/java/com/asb/aforo/dto/response/AforoTypesResponseDTO.java
+++ aforo/src/main/java/com/asb/aforo/dto/response/AforoTypesResponseDTO.java
@@ -1,17 +1,14 @@
 package com.asb.aforo.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class AforoTypesResponseDTO {
 
     private Long aforoTypeId;
     private String name;
 
-    public AforoTypesResponseDTO(Long aforoTypeId , String name){
-        this.aforoTypeId = aforoTypeId;
-        this.name = name;
-    }
 }
