7d92109d181f69e5e856f8005ddec72725c5b276
Alejandro Ceron <alejandrocg@switchs.co>
2024-04-08 10:13:07 -0500

    added funcionality to get a list of not collected posibilitys

--- aforo/src/main/java/com/asb/aforo/business/interfaces/NotCollectedInterfaceBusiness.java
+++ aforo/src/main/java/com/asb/aforo/business/interfaces/NotCollectedInterfaceBusiness.java
@@ -1,8 +1,12 @@
 package com.asb.aforo.business.interfaces;
 
+import com.asb.aforo.dto.response.NotCollectedResponseDTO;
 import com.asb.aforo.model.GgpNotCollected;
+
+import java.util.List;
 
 public interface NotCollectedInterfaceBusiness {
 
-    GgpNotCollected insertNotcollectedTable(GgpNotCollected NotCollected);
+    GgpNotCollected insertNotcollectedTable(GgpNotCollected notCollected);
+    List<NotCollectedResponseDTO> getNotCollected();
 }
--- aforo/src/main/java/com/asb/aforo/controller/NotCollectedController.java
+++ aforo/src/main/java/com/asb/aforo/controller/NotCollectedController.java
@@ -1,24 +1,29 @@
 package com.asb.aforo.controller;
 
 import com.asb.aforo.business.interfaces.NotCollectedInterfaceBusiness;
+import com.asb.aforo.dto.response.NotCollectedResponseDTO;
 import com.asb.aforo.model.GgpNotCollected;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/collected")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class NotCollectedController {
 
-    private final NotCollectedInterfaceBusiness business;
+    private final NotCollectedInterfaceBusiness notcollectedbusiness;
     @PostMapping("/insert")
     public ResponseEntity<GgpNotCollected> insertNotCollected(@RequestBody GgpNotCollected ggpNotCollected){
-        return new ResponseEntity<>(business.insertNotcollectedTable(ggpNotCollected), HttpStatus.OK);
+        return new ResponseEntity<>(notcollectedbusiness.insertNotcollectedTable(ggpNotCollected), HttpStatus.OK);
+    }
+
+    @GetMapping("/getNotCollected")
+    public ResponseEntity<List<NotCollectedResponseDTO>>getNotCollected(){
+        return new ResponseEntity<>(notcollectedbusiness.getNotCollected(),HttpStatus.OK);
     }
 }
